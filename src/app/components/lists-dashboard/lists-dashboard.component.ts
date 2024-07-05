117093d66b8cba3b202bb113d63a999329dfb519
PacoLapLinux <paco.morando@gmail.com>
2024-07-05 13:54:41 -0600

    trabajando en los diferentes tipos de listas

--- src/app/components/lists-dashboard/lists-dashboard.component.ts
+++ src/app/components/lists-dashboard/lists-dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ChatListComponent } from '../chat-list/chat-list.component';
+import { CheckListComponent } from '../check-list/check-list.component';
+import { ExpensesListComponent } from '../expenses-list/expenses-list.component';
 
 @Component({
   selector: 'app-lists-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ChatListComponent, CheckListComponent, ExpensesListComponent],
   templateUrl: './lists-dashboard.component.html',
   styleUrl: './lists-dashboard.component.css'
 })
